df8142301985fd0c166caf8de0008880856de3de
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2022-04-26 09:08:10 +0200

    SubmitYourSong_EQLIPS_V1

---
+++ sheets.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Form GSheets</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+    <div class="confirmation" id="confirmation">
+          T'as prod a bien été envoyé bg ! <br/> <br/>
+          Rejoins nous sur <a href="https://www.twitch.tv/namsco_" target="_blank">Twitch</a> <br/><br/>
+          <br/>
+          <a href="index.html">
+              <button class="button">Retour</button>
+          </a>
+    </div>
+
+    <div class="logo">
+        <img src="media/img/logo.png" alt="logo">
+    </div>
+
+    <?php
+
+        $name = $_POST['name'];
+        $link = $_POST['link'];
+        $insta = $_POST['insta'];
+        $message = $_POST['message'];
+
+        require __DIR__ . '/vendor/autoload.php';
+
+        $client = new \Google_Client();
+        $client -> setApplicationName('Google Sheets and PHP');
+        $client -> setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+        $client -> setAccessType('offline');
+        $client -> setAuthConfig(__DIR__ . "/credentials.json");
+
+        $service = new Google_Service_Sheets($client);
+        $spreadsheetId = "1rgQRABB1St2nuEKJ4crQB7pR_ZCvlCRU-IfU8Z6Z8aI";
+
+        $range = "page1";
+        
+        $values = [
+            [$name, $insta, $link, $message ]
+        ];
+        $body = new Google_Service_Sheets_ValueRange([
+            'values' => $values
+        ]);
+        $params = [
+            'valueInputOption' => 'RAW'
+        ];
+        $insert = [
+            "insertDataOption" => "INSERT_ROWS"
+        ];
+
+        $result = $service->spreadsheets_values->append(
+            $spreadsheetId,
+            $range,
+            $body,
+            $params,
+            $insert
+        );
+    ?>
+
+</body> 
+</html>
